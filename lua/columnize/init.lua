6278ebf4c9a9bd88cf56e5e4d5cc1b90c6397d25
Jakob Ruhe <jakob.ruhe@gmail.com>
2024-01-12 17:25:32 +0100

    Add support for user to supply a fixed column width
    By supplying an argument when invoking the user command `Columnize` a
    user is now able to specify the fixed column width.

--- lua/columnize/init.lua
+++ lua/columnize/init.lua
@@ -3,28 +3,32 @@ local strings = require('columnize.strings')
 
 local M = {}
 
-function M.columnize_lines(buffer, start_line, end_line)
+function M.columnize_lines(buffer, start_line, end_line, column_width)
     local lines_in = editor.get_lines(buffer, start_line, end_line)
     local indent = strings.left_whitespace(lines_in[1])
     local max_len = strings.max_word_length_in_lines(lines_in)
-    local lines_out = strings.columnize_lines(lines_in, indent, max_len)
+    if column_width == nil then
+        column_width = max_len
+    end
+    local lines_out = strings.columnize_lines(lines_in, indent, column_width)
     editor.set_lines(buffer, start_line, end_line, lines_out)
 end
 
 function M.columnize()
     local buffer = 0
     local start_line, end_line = editor.get_selected_lines()
-    M.columnize_lines(buffer, start_line, end_line)
+    M.columnize_lines(buffer, start_line, end_line, nil)
 end
 
 function M.columnize_cmd(ctx)
     local buffer = 0
     local start_line, end_line = ctx.line1, ctx.line2
-    M.columnize_lines(buffer, start_line, end_line)
+    local column_width = tonumber(ctx.args)
+    M.columnize_lines(buffer, start_line, end_line, column_width)
 end
 
 function M.setup(...)
-    vim.api.nvim_create_user_command('Columnize', M.columnize_cmd, {range=true})
+    vim.api.nvim_create_user_command('Columnize', M.columnize_cmd, { range = true, nargs = '?' })
 end
 
 return M
